74b424d4e21909628e742a1e3d2f357c4fedfd4b
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-27 19:07:46 +0100

    Classy exercises for learning the .net framework, constructors and static methods and properties

--- moreClassy/Program.cs
+++ moreClassy/Program.cs
@@ -10,6 +10,15 @@ namespace moreClassy
     {
         static void Main(string[] args)
         {
+
+
         }
+    }
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
     }
 }
